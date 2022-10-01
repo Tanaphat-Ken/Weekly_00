531c92485a04c9f66d8cc06ccd4f390d6af02afd
Tanaphat-Ken <kengodgood@gmail.com>
2022-10-01 13:03:58 +0700

    1

--- week 8/01.c
+++ week 8/01.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
-//
+//รับค่าตัวเลข 2 จำนวน เก็บใน x และ y ตามลำดับ จากนั้นให้ทำการสลับค่ากัน โดยห้ามสร้างตัวแปรเพิ่ม (Level 5)
 
 int main()
 {
-    printf("HELLO");
+    int x,y;
+    printf("ENTER X AND Y : ");
+    scanf("%d %d",&x,&y);
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    printf("SWAPPED : %d %d",x,y);
     return 0;
 }
